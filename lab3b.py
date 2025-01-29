b7124daa99dd9a8b5457b269c2d0fa1c33c50308
Rojina Bhandari <rbhandari17@myseneca.ca>
2025-01-28 19:57:33 -0500

    Lab completed

--- lab3b.py
+++ lab3b.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 '''Lab 3 Part 1 script - functions'''
 # Author ID: rbhandari17@myseneca.ca
-def describe_temperature(temp):
-    if temp > 30:
-        return 'hot'
-    elif temp < 0:
-        return 'cold'
-    elif temp == 20:
-        return 'perfect'
-    return 'ok'
-print(describe_temperature(50))   # Should return 'hot'
-print(describe_temperature(20))   # Should return 'perfect'
-print(describe_temperature(-50))  # Should return 'cold'
-print(describe_temperature(25))   # Should return 'ok'
-print(describe_temperature(10))   # Should return 'ok'
+
+def sum_numbers(number1, number2):
+    # Adds number1 and number2 and returns the value
+    return number1 + number2
+
+def subtract_numbers(number1, number2):
+    # Subtracts number2 from number1 and returns the value
+    return number1 - number2
+
+def multiply_numbers(number1, number2):
+    # Multiplies number1 and number2 and returns the value
+    return number1 * number2
+
+if __name__ == '__main__':
+    print(sum_numbers(10, 5))  # Output: 15
+    print(subtract_numbers(10, 5))  # Output: 5
+    print(multiply_numbers(10, 5))  # Output: 50
