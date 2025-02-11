1707b933634dfd50e7ddb9a3b445561de8166717
Rojina Bhandari <rbhandari17@myseneca.ca>
2025-02-11 17:07:01 -0500

    lab file  added

--- lab3c.py
+++ lab3c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Lab 3 Inv 2 function operate '''
-# Author ID: rbhandari17@myseneca.ca
+# Author ID:rbhandari17@myseneca.ca
 
 def operate(number1, number2, operator):
     if operator == 'add':
